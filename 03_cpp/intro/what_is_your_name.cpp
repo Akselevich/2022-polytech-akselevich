73655752096678e2254155321cc29ee6fc696325
Akselevich <112750440+Akselevich@users.noreply.github.com>
2022-10-14 10:59:14 +0300

    Create what_is_your_name.cpp

---
+++ 03_cpp/intro/what_is_your_name.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "What is your name?\n";
+    cin >> name;
+    cout<<"Your name is " << name <<endl;
+
+    return 0;
+}
